1faea6bd5bbe73dc364c9db185ce559429da0d37
Knight-Man298 <119781278+Knight-Man298@users.noreply.github.com>
2023-02-07 16:12:25 -0700

    contructor with params and getters/setters added

--- Assignment_1/Appliance.cs
+++ Assignment_1/Appliance.cs
@@ -8,9 +8,38 @@ namespace Assignment_1
 {
     internal class Appliance
     {
-        public Appliance()
-        {
+        private string id;
+        private string brand;
+        private double quantity;
+        private double wattage;
+        private string color;
+        private double price;
+        private double capacity;
+        private string roomType;
 
+        public string Id { get => id; set => id = value; }
+        public string Brand { get => brand; set => brand = value; }
+        public double Quantity { get => quantity; set => quantity = value; }
+        public double Wattage { get => wattage; set => wattage = value; }
+        public string Color { get => color; set => color = value; }
+        public double Price { get => price; set => price = value; }
+        public double Capacity { get => capacity; set => capacity = value; }
+        public string RoomType { get => roomType; set => roomType = value; }
+
+        public Appliance(string id, string brand, double quantity, double wattage, string color, double price, double capacity, string roomType)
+        {
+            Id = id;
+            Brand = brand;
+            Quantity = quantity;
+            Wattage = wattage;
+            Color = color;
+            Price = price;
+            Capacity = capacity;
+            RoomType = roomType;
         }
+
+
+
+
     }
 }
